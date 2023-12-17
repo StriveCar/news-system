5ac1ab7582ed7050b033a923c4cf10c6f52c3145
StriveCar <108204236+StriveCar@users.noreply.github.com>
2023-12-17 23:05:26 +0800

    增加新闻编辑申请

---
+++ src/main/java/com/news/NS/domain/dto/ApplicationListQueryDTO.java
@@ -0,0 +1,28 @@
+package com.news.NS.domain.dto;
+
+import lombok.Data;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author 车
+ * @date 2023/12/17 20 43
+ * discription
+ */
+@Data
+@ToString
+public class ApplicationListQueryDTO {
+    @Min(1)
+    private Integer page;
+
+    @Range(min = 1, max = 100)
+    private Integer size;
+
+    private Byte status;
+    private Integer userId;
+    private String reason;
+    private String name;
+}
